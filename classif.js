cd1dd02737ab8a878a9d458ed90146c7e11bcceb
AleksanderTh <acesarms42@gmail.com>
2024-07-13 20:40:21 -0300

    meu primeiro projeto

---
+++ classif.js
@@ -0,0 +1,48 @@
+let nome = "Aleksander"
+let XP = 6500
+
+
+if
+(XP <=1000)
+{
+nivel="Ferro"
+}
+else if
+(XP <=2000)
+{
+nivel ="Bronze"
+}
+else if
+(XP <=6000)
+{
+nivel="Prata"
+}
+else if
+(XP <=7000)
+{
+nivel ="Ouro"
+}
+
+else if
+(XP <=8000)
+{
+nivel="Platina"
+}
+else if
+(XP <=9000)
+{
+nivel ="Ascendente"
+}
+else if
+(XP <=10000)
+{
+nivel="Imortal"
+}
+else if
+(XP <=10001)
+{
+nivel ="Radiante"
+}
+
+
+console.log(`Nosso Heroi tem o nome ${nome}, e está no Nível ${nivel}. Parabéns!!!`)
